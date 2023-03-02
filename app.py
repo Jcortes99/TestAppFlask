5325687630910d9f4b360a5ae4cdf78efdf79153
jcortes-intecol <jcortes@intecol.com.co>
2023-03-02 12:00:42 -0500

    many changes
    Migration to postgresql
    and little changes in the front

--- app.py
+++ app.py
@@ -1,17 +1,44 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+app.config['AQLALCHEMY_DATABASE_URI']='postgresql://postgres:pasword@localhost/students'
+
+db=SQLAlchemy(app)
+
+class Student(db.Model):
+    __tablename__='students'
+    id=db.Column(db.Integer, primary_key=True)
+    fname=db.Column(db.String(200))
+    lname=db.Column(db.String(200))
+    pet=db.Column(db.String(200))
+
+    def __init__(self, fname, lname, pet):
+        self.fname=fname
+        self.lname=lname
+        self.pet=pet
+
+
+@app.route('/')
+def index():
     return render_template('index.html')
 
-@app.route("/home")
-def home():
-    return render_template("home.html")
+@app.route('/submit', methods=['GET','POST'])
+def submit():
+    if request.method == 'POST':
+        fname= request.form['fname']
+        lname= request.form['lname']
+        pet=request.form['pets']
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+        student=Student(fname,lname,pet)
+        db.session.add(student)
+        db.session.commit()
 
-if __name__ == "__main__":
-    app.run()
+        studentResult=db.session.query(Student).filter(Student.id==1)
+
+        for result in studentResult:
+            
+
+if __name__=='__main__':
+    app.run(debug=True)
